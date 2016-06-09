210fcb089e2f5d64ab4e509dde5351a11fb371a9
khongchi <sungbum00@gmail.com>
2016-06-09 13:38:25 +0900

    viewport 추가

--- theme/views/theme.blade.php
+++ theme/views/theme.blade.php
@@ -1,5 +1,10 @@
 @extends($theme::view('frame'))
 
+{{-- meta(viewport) --}}
+{{ app('xe.frontend')->meta()->name('viewport')->content(
+    'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no'
+)->load() }}
+
 {{-- stylesheet --}}
 {{ app('xe.frontend')->css([
     $theme::asset('css/owl.carousel.css'),
