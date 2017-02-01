125cbf042c52ddfc39abc587d468e64939047c4b
sungbum hong <sungbum00@gmail.com>
2017-02-01 18:19:34 +0900

    fix bug

--- theme/views/gnb.blade.php
+++ theme/views/gnb.blade.php
@@ -2,27 +2,27 @@
     @foreach(menu_list($config->get('mainMenu')) as $menu)
 
     {{-- depth0--}}
-    <li class="sub-menu @if($menu['selected']) on @endif ">
+    <li class=" @if(count($menu['children'])) sub-menu @endif @if($menu['selected']) on @endif ">
         <a href="{{ url($menu['url']) }}">
             {{ $menu['link'] }}
             @if(count($menu['children'])) <em class="ico-arrow"></em> @endif
         </a>
 
         @if(count($menu['children']))
-            <ul  class="sub-menu-list">
+            <ul class="sub-menu-list" style="display: none;">
                 @foreach($menu['children'] as $menu1)
                     {{-- depth1 --}}
-                    <li class="sub-menu @if($menu1['selected']) on @endif ">
+                    <li class=" @if(count($menu1['children'])) sub-menu @endif @if($menu1['selected']) on @endif ">
                         <a href="{{ url($menu1['url']) }}">
                             {{ $menu1['link'] }}
                             @if(count($menu1['children'])) <em class="ico-arrow"></em> @endif
                         </a>
 
                         @if(count($menu1['children']))
-                            <ul class="depth3">
+                            <ul class="sub-menu-list" style="display: none;">
                                 @foreach($menu1['children'] as $menu2)
                                     {{-- depth2 --}}
-                                    <li class="sub-menu @if($menu2['selected']) on @endif ">
+                                    <li class="@if(count($menu2['children'])) sub-menu @endif @if($menu2['selected']) on @endif ">
                                         <a href="{{ url($menu2['url']) }}">{{ $menu2['link'] }}</a>
                                     </li>
                                 @endforeach
