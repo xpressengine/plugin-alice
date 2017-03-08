948c7b47044946f4adade2c9686c03b7f60379a3
khongchi <sungbum00@gmail.com>
2017-03-08 11:50:13 +0900

    https://github.com/xpressengine/xpressengine/issues/392 fixed

--- theme/views/gnb.blade.php
+++ theme/views/gnb.blade.php
@@ -1,9 +1,9 @@
 <ul class="nav-list">
     @foreach(menu_list($config->get('mainMenu')) as $menu)
 
-    {{-- depth0--}}
+    {{-- depth0 --}}
     <li class=" @if(count($menu['children'])) sub-menu @endif @if($menu['selected']) on @endif ">
-        <a href="{{ url($menu['url']) }}">
+        <a href="{{ url($menu['url']) }}" target="{{ $menu['target'] }}">
             {{ $menu['link'] }}
             @if(count($menu['children'])) <em class="ico-arrow"></em> @endif
         </a>
@@ -13,7 +13,7 @@
                 @foreach($menu['children'] as $menu1)
                     {{-- depth1 --}}
                     <li class=" @if(count($menu1['children'])) sub-menu @endif @if($menu1['selected']) on @endif ">
-                        <a href="{{ url($menu1['url']) }}">
+                        <a href="{{ url($menu1['url']) }}" target="{{ $menu1['target'] }}">
                             {{ $menu1['link'] }}
                             @if(count($menu1['children'])) <em class="ico-arrow"></em> @endif
                         </a>
@@ -23,7 +23,7 @@
                                 @foreach($menu1['children'] as $menu2)
                                     {{-- depth2 --}}
                                     <li class="@if(count($menu2['children'])) sub-menu @endif @if($menu2['selected']) on @endif ">
-                                        <a href="{{ url($menu2['url']) }}">{{ $menu2['link'] }}</a>
+                                        <a href="{{ url($menu2['url']) }}" target="{{ $menu2['target'] }}">{{ $menu2['link'] }}</a>
                                     </li>
                                 @endforeach
                             </ul>
